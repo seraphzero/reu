c2741fdaa66f2a5f58c2090308cbda20f9db9213
Andrew Tieu <transcendentalavatar@gmail.com>
2015-07-30 13:48:53 -0500

    added basic admin functionality for models

--- reu/forum/admin.py
+++ reu/forum/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import Forum, Thread, Post
 
 # Register your models here.
+
+class ForumAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+class PostInline(admin.TabularInline):
+    model = Post
+
+class ThreadAdmin(admin.ModelAdmin):
+    list_display = ('title', 'created_by', 'created_on')
+    inlines = [PostInline]
+
+# class PostAdmin(admin.ModelAdmin):
+#     list_display = ('body', 'created_by', 'created_on')
+
+admin.site.register(Forum, ForumAdmin)
+admin.site.register(Thread, ThreadAdmin)
+# admin.site.register(Post, PostAdmin)
